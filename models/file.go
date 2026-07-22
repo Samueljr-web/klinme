7138b6019af735d5cd06218c93dd3dbbea98d0b8
Samuel Adeyemi <samueladeyemi.me@gmail.com>
2026-07-22 04:30:38 +0100

    feat: added models

---
+++ models/file.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type FileStatus string
+
+const (
+	FileStatusPending    FileStatus = "pending"
+	FileStatusProcessing FileStatus = "processing"
+	FileStatusDone       FileStatus = "done"
+	FileStatusFailed     FileStatus = "failed"
+)
+
+type File struct {
+	ID string `json:"id"`
+	UserId string `json:"user_id"`
+	FileName string `json:"file_name"`
+	FileSize int64 `json:"file_size"`
+	FileType string `json:"file_type"`
+	Status FileStatus `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type User struct{
+	ID string `json:"id"`
+	ClerkUserID string `json:"clerk_user_id"`
+	Email string `json:"email"`
+	Plan string `json:"plan"`
+	CleansUsed int `json:"cleans_used"`
+	CreatedAt time.Time `json:"created_at"`
+}
