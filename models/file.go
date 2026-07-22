38a1251c47edcb4f4b8e1a55db682b0c20a389c4
Samuel Adeyemi <samueladeyemi.me@gmail.com>
2026-07-22 04:52:40 +0100

    feat: format models

--- models/file.go
+++ models/file.go
@@ -12,11 +12,11 @@ const (
 )
 
 type File struct {
-	ID string `json:"id"`
-	UserId string `json:"user_id"`
-	FileName string `json:"file_name"`
-	FileSize int64 `json:"file_size"`
-	FileType string `json:"file_type"`
-	Status FileStatus `json:"status"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        string     `json:"id"`
+	UserID    string     `json:"user_id"`
+	FileName  string     `json:"file_name"`
+	FileSize  int64      `json:"file_size"`
+	FileType  string     `json:"file_type"`
+	Status    FileStatus `json:"status"`
+	CreatedAt time.Time  `json:"created_at"`
 }
--- models/user.go
+++ models/user.go
@@ -2,11 +2,11 @@ package models
 
 import "time"
 
-type User struct{
-	ID string `json:"id"`
-	ClerkUserID string `json:"clerk_user_id"`
-	Email string `json:"email"`
-	Plan string `json:"plan"`
-	CleansUsed int `json:"cleans_used"`
-	CreatedAt time.Time `json:"created_at"`
+type User struct {
+	ID          string    `json:"id"`
+	ClerkUserID string    `json:"clerk_user_id"`
+	Email       string    `json:"email"`
+	Plan        string    `json:"plan"`
+	CleansUsed  int       `json:"cleans_used"`
+	CreatedAt   time.Time `json:"created_at"`
 }
